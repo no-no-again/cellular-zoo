af806987183fb910f30b687b8a7aaa52f0b4e4e8
Mikhail Nevzorov <mikhail.zronev@gmail.com>
2022-10-22 15:12:08 +0400

    vsync and background color

--- main.go
+++ main.go
@@ -3,17 +3,21 @@ package main
 import (
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
 )
 
 func run() {
 	cfg := pixelgl.WindowConfig{
 		Title:  "Pixel Rocks!",
 		Bounds: pixel.R(0, 0, 400, 400),
+		VSync:  true,
 	}
 	win, err := pixelgl.NewWindow(cfg)
 	if err != nil {
 		panic(err)
 	}
+
+	win.Clear(colornames.Snow)
 
 	for !win.Closed() {
 		win.Update()
