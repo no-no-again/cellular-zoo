2b79aba47fcd924a17d66321c5d9762cbaada20e
Mikhail Nevzorov <mikhail.zronev@gmail.com>
2022-10-22 15:45:54 +0400

    draw rectangles

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"image/color"
+
 	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
 	"golang.org/x/image/colornames"
 )
 
+const (
+	windowWidth  = 600
+	windowHeight = 600
+	cellSize     = 10
+)
+
 func run() {
 	cfg := pixelgl.WindowConfig{
-		Title:  "Pixel Rocks!",
-		Bounds: pixel.R(0, 0, 400, 400),
+		Title:  "Cellular Zoo",
+		Bounds: pixel.R(0, 0, windowWidth, windowHeight),
 		VSync:  true,
 	}
 	win, err := pixelgl.NewWindow(cfg)
@@ -17,11 +26,26 @@ func run() {
 		panic(err)
 	}
 
-	win.Clear(colornames.Snow)
+	imd := imdraw.New(nil)
 
 	for !win.Closed() {
+		win.Clear(colornames.Snow)
+		imd.Clear()
+
+		drawRect(imd, windowWidth/2, windowHeight/2, cellSize, cellSize, colornames.Tomato)
+		drawRect(imd, 20, 20, cellSize, cellSize, colornames.Wheat)
+
+		imd.Draw(win)
 		win.Update()
 	}
+}
+
+func drawRect(imd *imdraw.IMDraw, x, y, w, h float64, color color.RGBA) {
+	rect := pixel.R(x, y, x+w, y+h).Norm()
+
+	imd.Color = color
+	imd.Push(rect.Min, rect.Max)
+	imd.Rectangle(0)
 }
 
 func main() {
