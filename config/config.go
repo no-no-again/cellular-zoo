c72770dcd0bca34edc3960faacfee7553530ea7d
Mikhail Nevzorov <mikhail.zronev@gmail.com>
2022-10-22 17:31:49 +0400

    cap frameRate

--- config/config.go
+++ config/config.go
@@ -1,6 +1,9 @@
 package config
 
 const (
-	WindowWidth = 800
+	WindowWidth  = 800
 	WindowHeight = 600
+
+	FPS       = 5.0
+	FrameRate = 1.0 / FPS
 )
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/zronev/cellular-zoo/colony"
 	"github.com/zronev/cellular-zoo/config"
 
@@ -30,12 +32,29 @@ func run() {
 	c := colony.New(config.WindowHeight/cellSize, config.WindowWidth/cellSize)
 	cd := colony.NewDrawer(c, cellSize)
 
+	update := func() {
+		c.NextGen()
+	}
+
+	draw := func() {
+		cd.Draw(imd)
+	}
+
+	timeStart := time.Now().UnixNano()
+
 	for !win.Closed() {
+		now := time.Now().UnixNano()
+		delta := float64(now-timeStart) / 1000000000
+
 		win.Clear(colornames.Snow)
 		imd.Clear()
 
-		cd.Draw(imd)
-		c.NextGen()
+		if delta >= config.FrameRate {
+			timeStart = now
+			update()
+		}
+
+		draw()
 
 		imd.Draw(win)
 		win.Update()
