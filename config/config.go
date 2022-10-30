3dc810eaa81d2dd8c3b724f4284b9226758061ee
Mikhail Nevzorov <mikhail.zronev@gmail.com>
2022-10-30 15:58:43 +0400

    change default config

--- config/config.go
+++ config/config.go
@@ -2,10 +2,10 @@ package config
 
 const (
 	WindowWidth   = 600
-	WindowHeight  = 600
-	SpawnCapacity = 0.8
-	FPS           = 8.0
+	WindowHeight  = 700
+	SpawnCapacity = 0.5
+	FPS           = 10.0
 	FrameRate     = 1.0 / FPS
-	CellSize      = 4
-	DefaultRule   = "3,5/4,7/5/M"
+	CellSize      = 5
+	DefaultRule   = "2-3/3/2/M"
 )
